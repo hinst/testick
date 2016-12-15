b08bee41fbcfa28f5da9bee192ffbbcade304b90
hinst <hinstance@yandex.ru>
2016-12-15 15:02:45 +0300

    Generator in progress

--- src/db_accessor/generator.go
+++ src/db_accessor/generator.go
@@ -1,15 +1,24 @@
 package db_accessor
 
-import "database/sql"
+import (
+	"bytes"
+	"database/sql"
+	"math/rand"
+	"time"
+)
 
 type TTestTableGenerator struct {
 	RowCount   int
+	TextSize   int
 	Connection *sql.DB
+	Random     *rand.Rand
 }
 
 func CreateTestTableGenerator() *TTestTableGenerator {
 	var result TTestTableGenerator
 	result.RowCount = 100 * 1000
+	result.TextSize = 1000
+	result.Random = rand.New(rand.NewSource(time.Now().UnixNano()))
 	return &result
 }
 
@@ -23,4 +32,22 @@ func (this *TTestTableGenerator) Generate() {
 func (this *TTestTableGenerator) CreateTable(transaction *sql.Tx) {
 	var _, executionResult = transaction.Exec("CREATE TABLE IF NOT EXISTS Notes (id INT, text TEXT);")
 	Assert(executionResult)
+}
+
+func (this *TTestTableGenerator) GenerateText() string {
+	var text bytes.Buffer
+	if this.Random.Int31n(10) == 0 {
+		text.WriteString("gold ")
+	}
+	for text.Len() < this.TextSize {
+		text.WriteString("text ")
+	}
+	return text.String()
+}
+
+func (this *TTestTableGenerator) GenerateRow(id int32) *TNoteRow {
+	var result = CreateNoteRow()
+	result.Id = id
+	result.Text = this.GenerateText()
+	return result
 }
--- src/db_accessor/noteRow.go
+++ src/db_accessor/noteRow.go
@@ -7,6 +7,11 @@ type TNoteRow struct {
 	Text string
 }
 
+func CreateNoteRow() *TNoteRow {
+	var result TNoteRow
+	return &result
+}
+
 func (this *TNoteRow) Insert(tableName string, transaction *sql.Tx) {
 	var _, executionResult = transaction.Exec(
 		"insert into "+tableName+"(id, text) values(?, ?)",
